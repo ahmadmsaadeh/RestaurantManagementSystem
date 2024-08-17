d450d1a2514328d61ba504f226c7034271027662
AlaaHasan12 <s12029009@stu.najah.edu>
2024-08-17 15:26:17 +0300

    Create Role API

---
+++ Backend/routes/role.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RolesController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+//for Admin:
+//Here we can get the role name to see the user role
+Route::get('/roles/{role_name}', [RolesController::class, 'getRoleByname']);
+
+//get all roles
+Route::get('/roles', [RolesController::class, 'getAllRoles']);
+
+//get role by id
+Route::get('/roles/role/{role_id}', [RolesController::class, 'getById']);
+
+//create role
+Route::post('/roles/create/role', [RolesController::class, 'createRole']);
+
+//delete role
+Route::delete('/roles/delete/{id}', [RolesController::class, 'deleteRole']);
+
+//update role
+Route::put('/roles/update/{id}', [RolesController::class, 'updateRole']);
