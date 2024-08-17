93cf632a3071a2adbe42f449ab6d37a40ec1f183
AlaaHasan12 <s12029009@stu.najah.edu>
2024-08-17 15:25:22 +0300

    Create User API

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -6,3 +6,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+require base_path('routes/auth.php');
+require base_path('routes/user.php');
+require base_path('routes/role.php');
