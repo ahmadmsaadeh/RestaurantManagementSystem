17c0578d9131021494341ff476d6df2f9a5f4fca
ahmadmsaadeh <asssa72412@gmail.com>
2024-08-18 19:46:58 +0300

    Adding Routs (missing Middleware to the routes for authentication)

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -1,15 +1,15 @@
 <?php
-//
-//use Illuminate\Http\Request;
-//use Illuminate\Support\Facades\Route;
-//
-//Route::get('/user', function (Request $request) {
-//    return $request->user();
-//})->middleware('auth:sanctum');
-//
-//require base_path('routes/order.php');
-//
-//require base_path('routes/auth.php');
-//require base_path('routes/user.php');
-//require base_path('routes/role.php');
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+require base_path('routes/order.php');
+
+require base_path('routes/auth.php');
+require base_path('routes/user.php');
+require base_path('routes/role.php');
 
