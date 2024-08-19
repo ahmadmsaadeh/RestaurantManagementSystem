f5a02e1413ed9168c48e54fbb42a08962d3d6fcf
ibtisamkharrosheh <ibtisamkharrosheh@gmail.com>
2024-08-19 19:32:37 +0300

    documentation for controller menu item

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -8,7 +8,12 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-// ibtisam
+/**
+ * @author Ibtisam
+ *
+ */
+
 Route::get('/menu-items', [MenuItemController::class, 'getMenuitem']);
 Route::get('/menu-items/{id}', [MenuItemController::class, 'getMenuItemById']);
-Route::post('/menu-items', [MenuItemController::class, 'createMenuItem']);
+Route::post('/menu-items', [MenuItemController::class, 'createMenuItem']);  // end
+
