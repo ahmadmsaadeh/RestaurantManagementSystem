c7c67fa2c0dfe211f8bfa7a9545037ff311d3c28
AlaaHasan12 <s12029009@stu.najah.edu>
2024-08-22 00:22:06 +0300

    create images table for chefs

--- Backend/routes/auth.php
+++ Backend/routes/auth.php
@@ -1,21 +1,23 @@
 <?php
 
 //all users login dynamically
+use App\Http\Controllers\LogInController;
+use App\Http\Controllers\LogOutController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
 Route::prefix('auth')->group(function () {
-    Route::get('login', [AuthController::class, 'login'])->name('login');
-    Route::get('logout',[AuthController::class, 'logout'])->name('logout');
+    Route::post('login', [LogInController::class, 'login'])->name('login');
+    Route::post('logout',[LogOutController::class, 'logout'])->name('logout');
     Route::post('forgetpassword', [AuthController::class, 'forgetpassword'])->name('forgetpassword');
 
     //here we have 2 type of user customer and staff ->
     // staff can't sign up directly the admin should give them there account so this one for customer
 
     //customer
-    Route::post('register/registercustomer', [AuthController::class, 'registercustomer']);
+    Route::post('registercustomer', [AuthController::class, 'registercustomer']);
 
     //staff
-    Route::post('register/registerstaff', [AuthController::class, 'registerStaff'])->name('register.staff');
+    Route::post('registerstaff', [AuthController::class, 'registerStaff'])->name('register.staff');
 
 });
