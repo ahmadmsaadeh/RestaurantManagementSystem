b18612e5f1572b915035c216e81cb2af4604c980
AlaaHasan12 <s12029009@stu.najah.edu>
2024-08-23 13:56:45 +0300

    Create API

--- Backend/app/Providers/AppServiceProvider.php
+++ Backend/app/Providers/AppServiceProvider.php
@@ -8,16 +8,20 @@ class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
+     *
+     * @return void
      */
-    public function register(): void
+    public function register()
     {
-        $this->app->register(\L5Swagger\L5SwaggerServiceProvider::class);
+        //
     }
 
     /**
      * Bootstrap any application services.
+     *
+     * @return void
      */
-    public function boot(): void
+    public function boot()
     {
         //
     }
