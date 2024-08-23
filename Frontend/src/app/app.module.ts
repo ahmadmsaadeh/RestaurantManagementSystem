f320a6eb30a606376bbd34bdc15cbafe3704fc67
ibtisamkharrosheh <ibtisamkharrosheh@gmail.com>
2024-08-23 22:41:08 +0300

    component menu_page

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuPageComponent } from './menu-page/menu-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuPageComponent
   ],
   imports: [
     BrowserModule,
