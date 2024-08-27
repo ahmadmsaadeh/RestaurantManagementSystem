de85206108b54e6fae2a4d3b91865b9b9ba88283
ahmadmsaadeh <asssa72412@gmail.com>
2024-08-27 16:37:10 +0300

    Reservation Updated

--- Frontend/src/app/reservation-table/reservation-table-service/reservation.service.ts
+++ Frontend/src/app/reservation-table/reservation-table-service/reservation.service.ts
@@ -7,10 +7,15 @@ import { Observable } from 'rxjs';
 })
 export class ReservationService {
   private apiUrl = 'http://localhost:8000/api/staff/reservations';
+  private userApiUrl = 'http://localhost:8000/api/users/';
 
   constructor(private http: HttpClient) {}
 
   getReservations(): Observable<any[]> {
     return this.http.get<any[]>(this.apiUrl);
+  }
+
+  getUser(userId: number): Observable<any> {
+    return this.http.get<any>(`${this.userApiUrl}${userId}`);
   }
 }
--- Frontend/src/app/user-reservation/user-reservation.component.ts
+++ Frontend/src/app/user-reservation/user-reservation.component.ts
@@ -1,6 +1,7 @@
-import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 import flatpickr from "flatpickr";
-import {english} from "flatpickr/dist/l10n/default";
+import { english } from "flatpickr/dist/l10n/default";
+import { GetUserReservationService } from './get-user-reservation/get-user-reservation.service';
 
 @Component({
   selector: 'app-user-reservation',
@@ -8,19 +9,23 @@ import {english} from "flatpickr/dist/l10n/default";
   styleUrls: ['./user-reservation.component.css']
 })
 export class UserReservationComponent implements AfterViewInit {
-@ViewChild('dateTimePicker') dateTimePicker!: ElementRef;
+  @ViewChild('dateTimePicker') dateTimePicker!: ElementRef;
+  @ViewChild('numberOfCustomers') numberOfCustomers!: ElementRef;
+  @ViewChild('reservationType') reservationType!: ElementRef;
 
-  constructor() { }
+  userId: number = 1;  // Suppose the user ID is set to 1
+
+  constructor(private reservationService: GetUserReservationService) {}
 
   ngAfterViewInit(): void {
     flatpickr(this.dateTimePicker.nativeElement, {
-    enableTime: true,
+      enableTime: true,
       dateFormat: 'Y-m-d H:i',
       locale: english,
       time_24hr: true,
       minuteIncrement: 10
-  });
-}
+    });
+  }
 
   openDateTimePicker(): void {
     const flatpickrInstance = (this.dateTimePicker.nativeElement as any)._flatpickr;
@@ -30,6 +35,17 @@ export class UserReservationComponent implements AfterViewInit {
   }
 
   onSubmit(): void {
-    console.log('Form submitted!');
+    const dateTime = this.dateTimePicker.nativeElement.value;
+    const numOfCustomers = this.numberOfCustomers.nativeElement.value;
+    const reservationType = this.reservationType.nativeElement.value;
+    const [date, time] = dateTime.split(' ');
+
+    this.reservationService.createReservation(this.userId, date, time, numOfCustomers, reservationType)
+      .subscribe(response => {
+        console.log('Reservation created successfully', response);
+      }, error => {
+        console.error('Error creating reservation', error);
+      });
   }
+
 }
