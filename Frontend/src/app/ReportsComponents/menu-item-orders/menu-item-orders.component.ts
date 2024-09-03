0ea8235a66bcacce23f278b8733df89b67f9e9a0
talamyaseen <talamyaseen@gmail.com>
2024-09-03 07:49:19 +0300

    Arranging folders

---
+++ Frontend/src/app/ReportsComponents/menu-item-orders/menu-item-orders.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { ReportingService } from '../../services/reporting.service';
+
+@Component({
+  selector: 'app-menu-item-orders',
+  templateUrl: './menu-item-orders.component.html'
+})
+export class MenuItemOrdersComponent implements OnInit {
+
+  menuItems: any[] = [];
+
+  constructor(private ReportingService: ReportingService) { }
+
+  ngOnInit(): void {
+    this.ReportingService.getMenuItemOrders().subscribe(data => {
+      this.menuItems = data;
+    });
+  }
+}
